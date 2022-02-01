473fdcbdd24b1c86afd4740e3f5787ae5990c6ba
Brian Dombrowski <bdombro@gmail.com>
2022-02-01 17:02:39 -0600

    Ignoring db files

--- src/api/plugins/auth.plugin.ts
+++ src/api/plugins/auth.plugin.ts
@@ -1,5 +1,7 @@
 import { AuthUsers } from '#src/pouch/databases'
 
+import config from '../lib/config.node'
+
 export default async function authorizationPlugin(app: FastifyInstance, options: FastifyOptions) {
 	app.addHook('onRequest', async function setAuthHeaders(req, reply) {
 		if (req.url.startsWith('_session')) {
@@ -24,16 +26,32 @@ export default async function authorizationPlugin(app: FastifyInstance, options:
 		// 	roles: ['_tenant']
 		// }
 		// const user = await AuthUsers.get('org.couchdb.user:sallyfields@hookedjs.org')
+		// TODO:
+		// 1. create user or return 401
+		const res = await fetch(
+			config.dbUrl + '/_session',
+			{
+				method: 'POST',
+				body: JSON.stringify(req.body)
+			}
+		)
+		const user = await res.json()
+		// 2. enhance user profile with more fields?
+		// 2. create user/tenant db?
+		// 3. Send email confirmation?
 		// @ts-ignore
-		const user = await AuthUsers.get(props.name)
+		// const user = await AuthUsers.get(props.name)
 		reply.send({data: user})
 	})
 
-	// app.get('/api/_session', async function getAuthStatusHandler(req, reply) {
-	// 	reply
-	// 		.headers({'cache-control': 'no-store, max-age=0'})
-	// 		.send(data)
-	// })
+	app.get('/api/_session', async function getAuthStatusHandler(req, reply) {
+		// TODO:
+		// 1. get user
+		// 2. check user status.
+		reply
+			.headers({'cache-control': 'no-store, max-age=0'})
+			.send(data)
+	})
 	// app.delete('/api/_session', async function deleteAuthHandler(req, reply) {
 	// 	const data = {}
 	// 	reply
